0e00893d14f970f5fba40d15653b5ca5ee343d41
sofibear01 <sofibearzotti01@gmail.com>
2023-08-03 15:16:49 -0300

    cambios clase 22

---
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -0,0 +1,77 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    private Integer id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Integer celular;
+
+    private String direccion;
+
+    private String correo_electronico;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Compra> compras;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Integer celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+}
